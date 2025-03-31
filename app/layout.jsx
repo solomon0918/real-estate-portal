b7bf987cae58dfc24aa95aeb6cc7fdb2e36a4ad2
Erwin Mark <anora.erwinmark@gmail.com>
2025-03-31 18:09:42 +0800

    first commit

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import AuthProvider from '@/components/AuthProvider';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+  title: 'Property Pulse',
+  keywords: 'rental, property, real estate',
+  description: 'Find the perfect rental property',
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <AuthProvider>
+      <html>
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+          <ToastContainer />
+        </body>
+      </html>
+    </AuthProvider>
+  );
+};
+
+export default MainLayout;
